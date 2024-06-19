2318bc597f06f76983bc36a55dcf3b2399f78b37
Mauricio Vazquez <mvazq109@itam.mx>
2024-06-18 20:24:41 -0600

    Auxiliar functions

--- WebScraper.py
+++ WebScraper.py
@@ -1,4 +1,4 @@
-# Libraries
+############ Libraries ############
 
 import re
 from io import StringIO
@@ -29,7 +29,9 @@ from urllib.error import URLError
 from urllib.request import Request
 import lxml
 
-# Code
+
+
+############ Code ############
 
 # Testing server answer
 try:
@@ -40,3 +42,100 @@ except URLError as e:
     print("The server could not be found!")
 else:
     print(html.read())
+
+
+#### Auxiliar functions ####
+
+# Getting title function
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bsObj = BeautifulSoup(html.read(), "lxml")
+        title = bsObj.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+
+# Number of pages per state function
+def numberOfpagesPerstate(bs):
+    numPags = bs.find('select', class_="sorting nativeDropdown js-pagination-dropdown").get('data-pagination-end')
+    if numPags is not None:
+        num = int(numPags)
+    else:
+        num = 0
+    return num
+
+
+# Creating list of Main URL's 
+def listMainURLs(nomState):
+    MainUrls = []
+    listURLs = list(MainUrls)
+    n = numberOfpagesPerstate(bs)
+    i=1
+    while i<=n:
+        aux = '........'+nomState+'........../?page='+ str(i)
+        listURLs.append(aux)
+        i+=1
+    return listURLs
+
+# Auxiliar function
+def lM(bs):
+    l = []
+    lista= list(l)
+
+    links = bs.find('div', class_ = "small-12 crosslinkTitle-links")
+    arr = links.find_all('a')
+    for pit in arr:
+        ka = pit.get('href')
+        lista.append(ka)
+    return lista
+
+
+#### Declaring variables ####
+
+# Our DataFrame
+df = pd.DataFrame()
+df['tipo'] = None
+df['precio'] = None
+df['currency'] = None
+df['metros_cuadrados'] = None
+df['direccion'] = None
+df['estado'] = None
+df['latitud'] = None
+df['longitud'] = None
+df['link'] = None
+
+
+# Temporal arrays
+tipo = []
+precio = []
+currency = []
+m2 = []
+units = []
+direccion = []
+estado = []
+latitud = []
+longitud = []
+link = []
+
+# States names list
+est = ['aguascalientes','baja-california', 
+       'baja-california-sur', 'campeche', 
+       'chiapas', 'chihuahua' , 
+       'coahuila-de-zaragoza', 'colima', 
+       'distrito-federal', 'durango', 
+       'guanajuato' , 'guerrero',  
+       'hidalgo', 'jalisco', 
+       'mexico' , 'michoacan-de-ocampo', 
+       'morelos', 'nayarit', 
+       'nuevo-leon', 'oaxaca', 
+       'puebla', 'queretaro-arteaga', 
+       'quintana-roo', 'san-luis-potosi', 
+       'sinaloa', 'sonora', 
+       'tabasco','tamaulipas',
+       'tlaxcala', 'veracruz-llave', 
+       'yucatan', 'zacatecas']
